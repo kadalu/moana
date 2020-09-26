806206a3c8e71837c18a10b41fe3ec495aac3e2c
Aravinda Vishwanathapura <aravinda@kadalu.io>
2020-09-26 10:51:02 +0530

    Default node name and endpoint
    * If `NODENAME` is not provided then take the nodename from
      the `hostname` command.
    * If `ENDPOINT` is not provided then take the endpoint from
      `http://$(hostname):$(settings.port)`. Use `https` if `ENDPOINT_HTTPS=yes`
    * If `PORT` is provided then use that for setting endpoint.
    * If `ENDPOINT` is provided then derive `settings.port` from the endpoint.
    * If both `ENDPOINT` and `PORT` are provided and if the port value
      doesn't match then ignore the `PORT` provided and use the `settings.port`.
    
    Signed-off-by: Aravinda Vishwanathapura <aravinda@kadalu.io>

--- moana-node/src/moana-node.cr
+++ moana-node/src/moana-node.cr
@@ -1,17 +1,28 @@
+require "log"
+
 require "../config/application"
 require "./watcher"
 
 node_name = ENV.fetch("NODENAME", "")
-if node_name == ""
-  STDERR.puts "Environment variable NODENAME is not provided"
-  exit 1
+# Set node_name as hostname itself
+node_name = `hostname`.strip if node_name == ""
+
+pfx = "http"
+endpoint_https = ENV.fetch("ENDPOINT_HTTPS", "")
+if endpoint_https == "yes"
+  pfx = "https"
 end
 
 node_endpoint = ENV.fetch("ENDPOINT", "")
 if node_endpoint == ""
-  STDERR.puts "Environment variable ENDPOINT is not provided"
-  exit 1
+  # Set hostname:PORT as endpoint if not set
+  node_endpoint = "#{pfx}://#{`hostname`.strip}:#{Amber.settings.port}"
+else
+  # Set Port same as specified in the ENDPOINT
+  Amber.settings.port = node_endpoint.split(":")[-1].to_i
 end
+
+Log.info {"Starting moana-node [{nodename=#{node_name}}, {endpoint=#{node_endpoint}}, {port=#{Amber.settings.port}}]"}
 
 watcher = Watcher.new
 watcher.start
