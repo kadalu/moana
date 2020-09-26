dbe5b6395a16b99ed6a7fd211c9f925588e843db
Aravinda Vishwanathapura <aravinda@kadalu.io>
2020-09-26 22:02:08 +0530

    Volume Start API
    Signed-off-by: Aravinda Vishwanathapura <aravinda@kadalu.io>

--- moana-node/src/controllers/volume_controller.cr
+++ moana-node/src/controllers/volume_controller.cr
@@ -9,7 +9,7 @@ end
 class BrickRequest
   include JSON::Serializable
 
-  property path : String, device : String, node : NodeRequest, mount_path : String = ""
+  property id : String = "", path : String, device : String, node : NodeRequest, mount_path : String = "", port : Int32 = 0
 end
 
 class VolumeRequest
@@ -24,26 +24,54 @@ class VolumeRequest
            ext4_opts : String = ""
 end
 
-def nodeid_from_conf
+class SubvolRequest
+  include JSON::Serializable
+
+  property name : String,
+           bricks : Array(BrickRequest)
+end
+
+class VolumeSubvolRequest
+  include JSON::Serializable
+
+  property id : String,
+           name : String,
+           subvols : Array(SubvolRequest),
+           brick_fs : String,
+           xfs_opts : String = "",
+           zfs_opts : String = "",
+           ext4_opts : String = ""
+end
+
+struct Volfile
+  include JSON::Serializable
+
+  property content : String
+end
+
+def nodedata_from_conf
   workdir = ENV.fetch("WORKDIR", "")
   filename = "#{workdir}/#{ENV["NODENAME"]}.json"
-  nodeid = ""
   if File.exists?(filename)
-    conf = NodeConfig.from_json(File.read(filename))
-    if tmp = conf.node_id
-      nodeid = tmp
-    end
+    NodeConfig.from_json(File.read(filename))
+  else
+    nil
   end
-
-  nodeid
 end
 
 class VolumeController < ApplicationController
   def create
-    this_node = nodeid_from_conf
+    this_node = nodedata_from_conf
+    if this_node.nil?
+      result = {error: "failed to get node configuration"}
+      return respond_with 500 do
+        json result.to_json
+      end
+    end
+
     volreq = VolumeRequest.from_json(params["data"])
     volreq.bricks.each do |brick|
-      next if this_node != brick.node.id
+      next if this_node.node_id != brick.node.id
 
       if brick.device != ""
         brick.mount_path = Path[brick.path].parent.to_s
@@ -58,6 +86,86 @@ class VolumeController < ApplicationController
       end
     end
 
+    respond_with 201 do
+      json "{\"ok\": true}"
+    end
+  end
+
+  def start
+    this_node = nodedata_from_conf
+    if this_node.nil?
+      result = {error: "failed to get node configuration"}
+      return respond_with 500 do
+        json result.to_json
+      end
+    end
+
+    volreq = VolumeSubvolRequest.from_json(params["data"])
+    volreq.subvols.each do |subvol|
+      subvol.bricks.each do |brick|
+        next if this_node.node_id != brick.node.id
+
+        if brick.device != ""
+          brick.mount_path = Path[brick.path].parent.to_s
+        end
+
+        # Download the Volfile
+        url = "#{this_node.moana_url}/api/volfiles/#{params["cluster_id"]}/brick/#{volreq.id}/#{brick.id}"
+        response = HTTP::Client.get url
+        content = "[]"
+        workdir = ENV.fetch("WORKDIR", "")
+
+        # Download and Create the Volfile
+        if response.status_code == 200
+          volfile = Volfile.from_json(response.body)
+
+          filename = "#{workdir}/volfiles/#{brick.node.hostname}:#{brick.path.gsub("/", "-")}.vol"
+          File.write(filename, volfile.content)
+        else
+          result = {error: "Failed to fetch Volfile: #{response.status_code}"}
+          return respond_with 500 do
+            json result.to_json
+          end
+        end
+
+        # Create the config file
+        filename = "#{workdir}/#{brick.node.hostname}:#{brick.path.gsub("/", "-")}.json"
+        File.write(filename, {
+                     "path" => brick.path,
+                     "node.id" => brick.node.id,
+                     "node.hostname" => brick.node.hostname,
+                     "volume.name" => volreq.name,
+                     "port" => brick.port,
+                     "device" => brick.device
+                   }.to_json)
+
+        # Enable the Service
+        ret, resp = execute(
+               "systemctl", [
+                 "enable",
+                 "kadalu-brick@#{brick.node.hostname}:#{brick.path.gsub("/", "-")}.service"
+               ])
+        if ret != 0
+          result = {error: "Failed to enable service: #{resp}"}
+          return respond_with 500 do
+            json result.to_json
+          end
+        end
+
+        # Start the Service
+        ret, resp = execute(
+               "systemctl", [
+                 "start",
+                 "kadalu-brick@#{brick.node.hostname}:#{brick.path.gsub("/", "-")}.service"
+               ])
+        if ret != 0
+          result = {error: "Failed to start service: #{resp}"}
+          return respond_with 500 do
+            json result.to_json
+          end
+        end
+      end
+    end
     respond_with 201 do
       json "{\"ok\": true}"
     end
