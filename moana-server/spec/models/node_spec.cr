c7d5b6124ff16b4d6599b2467b5543246e354438
Aravinda Vishwanathapura <aravinda@kadalu.io>
2020-12-27 20:50:56 +0530

    server: Delete the Old server implementation
    Signed-off-by: Aravinda Vishwanathapura <aravinda@kadalu.io>

--- moana-server/spec/models/node_spec.cr
+++
@@ -1,8 +0,0 @@
-require "./spec_helper"
-require "../../src/models/node.cr"
-
-describe Node do
-  Spec.before_each do
-    Node.clear
-  end
-end
--- moana-server/src/controllers/application_controller.cr
+++
@@ -1,6 +0,0 @@
-require "jasper_helpers"
-
-class ApplicationController < Amber::Controller::Base
-  include JasperHelpers
-  LAYOUT = false
-end
