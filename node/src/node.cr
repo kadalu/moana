2c97ffd0cbbb6f64116bd2ffdd4f45348082126f
Aravinda Vishwanathapura <aravinda@kadalu.io>
2020-12-27 20:59:13 +0530

    node: Node configuration management
    Before joining to any Cluster, just after node agent is
    started, the following Configs will be available via
    environment variables or by default configurations.
    
    * **workdir** - Working directory for managing config files
      and temporary files. env.WORKDIR or default(`/var/lib/kadalu`)
    * **hostname** - Hostname to use in all Operations, Volfiles etc.
      `env.HOSTNAME` or default(`hostname` command).
    * **port** - Port for Node agent. `env.PORT` or default(`5001`).
    * **endpoint_https** - If HTTPS is to be used in all internal
      communication. `env.ENDPOINT_HTTPS` or default(`no`).
    * **endpoint** - Endpoint that other nodes within the Cluster will
      use for internal communication. `env.ENDPOINT` or
      default(<endpoint-prefix>://<hostname>:<port>).
      `endpoint_prefix=endpoint_https == "yes" ? "https" : "http"`
    
    Once the Node joins to a Cluster then the following additional
    configurations will be available.
    
    * **cluster_id** - ID of the Cluster to which node is joined.
    * **node_id** - Node ID assigned by the Moana Server. Use this
      in all future communications or while getting the Task lists.
    * **moana_url** - Moana Server URL.
    * **token** - Token provided by the Moana Server once node joined
      to the Cluster. Use this token in all future communications with
      Moana Server.
    
    Signed-off-by: Aravinda Vishwanathapura <aravinda@kadalu.io>

--- node/src/node.cr
+++ node/src/node.cr
@@ -1,8 +1,14 @@
 require "kemal"
 
+require "./node_conf"
+
+# Initialize the node configuration
+node_conf = NodeConf.new
+
 # Run the Web service to receive Tasks from different nodes.
 # Moana server will not contact the node agent directly. Instead,
 # each node agent pulls the list of tasks assigned to respective
 # node and then broadcast to all Participating nodes within the
 # Cluster and updates the response back to Moana Server.
+Kemal.config.port = node_conf.port
 Kemal.run
