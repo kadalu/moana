a451cbcfaeb3b3851aa7bc57a5c304ee5d499b7f
Aravinda Vishwanathapura <aravinda@kadalu.io>
2020-12-28 15:44:38 +0530

    release: Static image builder scripts (#10)
    * Script added to build Static binaries
    * Build time is very slow for release builds
    * Install and usage instructions updated to README.
    
    Signed-off-by: Aravinda Vishwanathapura <aravinda@kadalu.io>

---
+++ build_arm64_static.sh
@@ -0,0 +1,38 @@
+#!/bin/sh -eu
+
+# Based on https://gist.github.com/j8r/34f1a344336901960c787517b5b6d616
+
+LOCAL_PROJECT_PATH=${1-$PWD}
+VERSION=${VERSION-devel}
+CMDS="
+echo '@edge http://dl-cdn.alpinelinux.org/alpine/edge/community' >>/etc/apk/repositories
+apk add --update --no-cache --force-overwrite \
+    crystal@edge \
+    gc-dev gcc gmp-dev libatomic_ops libevent-static musl-dev pcre-dev \
+    libxml2-dev openssl-dev openssl-libs-static tzdata yaml-dev zlib-static \
+    make git \
+    llvm10-dev llvm10-static g++ \
+    shards@edge \
+    yaml-static \
+    sqlite-dev sqlite-static
+cd server
+shards install --production
+VERSION=${VERSION} time -v shards build --static --release --stats --time
+chown 1000:1000 -R bin
+mv bin/moana-server bin/moana-server-arm64
+cd ../node
+shards install --production
+VERSION=${VERSION} time -v shards build --static --release --stats --time
+chown 1000:1000 -R bin
+mv bin/moana-node bin/moana-node-arm64
+cd ../cli
+shards install --production
+VERSION=${VERSION} time -v shards build --static --release --stats --time
+chown 1000:1000 -R bin
+mv bin/moana bin/moana-arm64
+"
+
+# Compile Crystal project statically for arm64 (aarch64)
+docker pull multiarch/qemu-user-static:register
+docker run --rm --privileged multiarch/qemu-user-static:register --reset
+docker run -it -v $LOCAL_PROJECT_PATH:/workspace -w /workspace --rm multiarch/alpine:aarch64-latest-stable /bin/sh -c "$CMDS"
--- build_linux64_static.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-time docker run --rm -it -v $PWD:/workspace -w /workspace crystallang/crystal:0.35.1-alpine /bin/sh -c 'cd cli && shards install --production && shards build --release --static'
-time docker run --rm -it -v $PWD:/workspace -w /workspace crystallang/crystal:0.35.1-alpine /bin/sh -c 'cd moana-server && shards install --production && shards build --release --static'
-time docker run --rm -it -v $PWD:/workspace -w /workspace crystallang/crystal:0.35.1-alpine /bin/sh -c 'cd moana-node && shards install --production && shards build --release --static'
