459f861a930f7b6cf412ae23448f49d500f95098
Aravinda Vishwanathapura <aravinda@kadalu.io>
2021-03-06 15:26:49 +0530

    Fix wrong volume type when pure distribute Volume was created
    * Wrong Calculation was used to find the number of Sub volumes from
      volume create request.
    * GLUSTERFSD env variable was not available for systemd service
    * Bitrot was not built but was used in brick graph.
    
    Signed-off-by: Aravinda Vishwanathapura <aravinda@kadalu.io>

--- extra/entrypoint_node.sh
+++ extra/entrypoint_node.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-systemctl set-environment GLUSTERFSD=/opt/sbin/glusterfsd
+mkdir -p /etc/systemd/system.conf.d/
 mkdir -p /var/lib/kadalu /var/run/kadalu /var/lib/kadalu/volfiles /var/log/kadalu
+
+echo "[Manager]
+DefaultEnvironment=$(while read -r Line; do echo -n "$Line " ; done < <(env))
+" > /etc/systemd/system.conf.d/myenvironment.conf
 
 exec "$@"
