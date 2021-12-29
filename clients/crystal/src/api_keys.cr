5f8560bf5a73133c665c52ada0541b77135854df
Aravinda Vishwanathapura <aravinda@kadalu.io>
2021-12-29 22:34:40 +0530

    API Key create and delete
    Also updated the user management documentation.
    
    Signed-off-by: Aravinda Vishwanathapura <aravinda@kadalu.io>

--- clients/crystal/src/api_keys.cr
+++ clients/crystal/src/api_keys.cr
@@ -3,6 +3,22 @@ module MoanaClient
     def initialize(@client : Client, @api_key_id : String)
     end
 
+    def self.create(client : Client, name : String)
+      url = "#{client.url}/api/v1/api-keys"
+
+      req = {"name": name}
+      response = MoanaClient.http_post(
+        url,
+        req.to_json,
+        headers: client.auth_header
+      )
+      if response.status_code == 201
+        MoanaTypes::ApiKey.from_json(response.body)
+      else
+        MoanaClient.error_response(response)
+      end
+    end
+
     def self.list(client : Client, user_id : String)
       url = "#{client.url}/api/v1/api-keys"
       response = MoanaClient.http_get(
@@ -12,6 +28,19 @@ module MoanaClient
       if response.status_code == 200
         Array(MoanaTypes::ApiKey).from_json(response.body)
       else
+        MoanaClient.error_response(response)
+      end
+    end
+
+    def delete
+      url = "#{@client.url}/api/v1/api-keys/#{@api_key_id}"
+
+      response = MoanaClient.http_delete(
+        url,
+        headers: @client.auth_header
+      )
+
+      if response.status_code != 204
         MoanaClient.error_response(response)
       end
     end
