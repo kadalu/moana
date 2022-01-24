9fd113cd1bd4dbce0fa6f3147fd3c86af460c64d
Aravinda Vishwanathapura <aravinda@kadalu.io>
2022-01-24 19:21:22 +0530

    Removed old iteration code (#161)
    Signed-off-by: Aravinda Vishwanathapura <aravinda@kadalu.io>

--- ref/extra/entrypoint_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# TODO: This is not working
-if [[ -z "${KADALU_MGMT_SERVER}" ]]; then
-    export KADALU_MGMT_SERVER=http://$(hostname):3000
-fi
-
-exec "$@"
--- ref/server/src/db/helpers.cr
+++
@@ -1,5 +0,0 @@
-require "digest/sha256"
-
-def hash_sha256(value : String)
-  Digest::SHA256.digest(value).hexstring
-end
