05113f04c23d58a418758faf62a17957fbe905bd
Shree Vatsa N <vatsa@kadalu.io>
2022-02-28 17:20:33 +0530

    Add --volume-id, vol-name checks
    Signed-off-by: Shree Vatsa N <vatsa@kadalu.io>

--- mgr/src/server/plugins/volume_expand.cr
+++ mgr/src/server/plugins/volume_expand.cr
@@ -6,14 +6,8 @@ require "../datastore/*"
 require "./ping"
 require "./volume_utils.cr"
 
-ACTION_VALIDATE_VOLUME_EXPAND = "validate_volume_create"
-ACTION_VOLUME_EXPAND          = "volume_create"
-ACTION_VOLUME_EXPAND_STOPPED  = "volume_create_stopped"
 
-# {"id":"92c2f6d3-21c0-41b7-9761-fa527cdf87e6","name":"kadalu-dev","state":"","pool":{"id":"","name":"v","nodes":[]},"distribute_groups":[{"storage_units":[{"id":"","port":0,"path":"/mnt/disk1","node":{"id":"6ea496e9-82b2-4ada-b2f1-ee3f7b1a67f4","name":"kadalu-dev","state":"","endpoint":"http://kadalu-dev:3000","addresses":[],"token":"adcbf323-f78d-48d4-8350-8429ca064ac5","pool":{"id":"d7d7f3b2-fa96-480b-a9c9-97df25211722","name":"v","nodes":[]}},"type":"","fs":"","metrics":{"size_bytes":0,"inodes_count":0,"size_used_bytes":0,"size_free_bytes":0,"inodes_used_count":0,"inodes_free_count":0,"health":""},"service":{"id":"","name":"","args":[],"pid_file":"","path":"","metrics":{"size_bytes":0,"inodes_count":0,"size_used_bytes":0,"size_free_bytes":0,"inodes_used_count":0,"inodes_free_count":0,"health":""},"wait":true,"create_pid_file":true}}],"replica_count":0,"arbiter_count":0,"disperse_count":0,"redundancy_count":0,"replica_keyword":"","metrics":{"size_bytes":0,"inodes_count":0,"size_used_bytes":0,"size_free_bytes":0,"inodes_used_count":0,"inodes_free_count":0,"health":""}}],"no_start":false,"volume_id":"","auto_create_pool":false,"auto_add_nodes":false,"options":{},"metrics":{"size_bytes":0,"inodes_count":0,"size_used_bytes":0,"size_free_bytes":0,"inodes_used_count":0,"inodes_free_count":0,"health":""},"snapshot_plugin":""}
-# #<MoanaTypes::Volume:0x7f65bd25e780>
-
-node_action ACTION_VALIDATE_VOLUME_EXPAND do |data, _env|
+node_action ACTION_VALIDATE_VOLUME_CREATE do |data, _env|
   req = MoanaTypes::Volume.from_json(data)
   puts data, req
   validate_volume_create(req)
@@ -39,9 +33,9 @@ put "/api/v1/pools/:pool_name/volumes" do |env|
 
   volume = Datastore.get_volume(pool_name, req.name)
   puts volume
-  #   unless volume.nil?
-  #     halt(env, status_code: 400, response: ({"error": "Volume already exists"}.to_json))
-  #   end
+  if volume.nil?
+    halt(env, status_code: 400, response: ({"error": "The Volume (#{req.name}) doesn't exists"}.to_json))
+  end
 
   pool = Datastore.get_pool(pool_name)
   if pool.nil?
@@ -58,7 +52,7 @@ put "/api/v1/pools/:pool_name/volumes" do |env|
   # req.id = req.volume_id == "" ? UUID.random.to_s : req.volume_id
 
   puts "volId:", volume.not_nil!.id
-  req.id = volume.not_nil!.id
+  req.id = req.volume_id == "" ? volume.not_nil!.id : req.volume_id
 
   # Checks for types, replica counts etc
   puts "type", volume.not_nil!.type, req.type
@@ -85,17 +79,16 @@ put "/api/v1/pools/:pool_name/volumes" do |env|
     end
   end
 
-  # TODO: Add --volume-id for expand
-  # if req.volume_id != "" && !valid_uuid?(req.volume_id)
-  #   halt(env, status_code: 400, response: ({"error": "Volume ID does not match UUID format"}.to_json))
-  # end
+  if req.volume_id != "" && !valid_uuid?(req.volume_id)
+    halt(env, status_code: 400, response: ({"error": "Volume ID does not match UUID format"}.to_json))
+  end
 
   # # To avoid creating existing volume with volume-id option
   # if req.volume_id != "" && Datastore.volume_exists_by_id?(pool.not_nil!.id, req.id)
   #   halt(env, status_code: 400, response: ({"error": "Volume already exists with the given ID"}.to_json))
   # end
 
-  # TODO: Validate the request, dist count, storage_units count etc
+  # TODO: Validate the request, dist count, storage_units count etc [done]
 
   nodes = [] of MoanaTypes::Node
 
@@ -108,32 +101,33 @@ put "/api/v1/pools/:pool_name/volumes" do |env|
     node = Datastore.get_node(pool_name, n.name)
     puts n.name
     if node.nil?
-      #   if req.auto_add_nodes
-      #     endpoint = node_endpoint(n.name)
-      #     invite = node_invite(pool_name, n.name, endpoint)
+      if req.auto_add_nodes
 
-      #     participating_node = MoanaTypes::Node.new
-      #     participating_node.endpoint = endpoint
-      #     participating_node.name = n.name
+        endpoint = node_endpoint(n.name)
+        invite = node_invite(pool_name, n.name, endpoint)
 
-      #     resp = dispatch_action(
-      #       ACTION_NODE_INVITE_ACCEPT,
-      #       pool_name,
-      #       [participating_node],
-      #       invite.to_json
-      #     )
+        participating_node = MoanaTypes::Node.new
+        participating_node.endpoint = endpoint
+        participating_node.name = n.name
 
-      #     if !resp.ok
-      #       invalid_reason = resp.node_responses[n.name].response
-      #       invalid_node = true
-      #       invalid_node_name = n.name
-      #       break
-      #     end
+        resp = dispatch_action(
+          ACTION_NODE_INVITE_ACCEPT,
+          pool_name,
+          [participating_node],
+          invite.to_json
+        )
 
-      #     node = MoanaTypes::Node.from_json(resp.node_responses[n.name].response)
-      #     node.endpoint = endpoint
-      #     Datastore.create_node(pool.not_nil!.id, node.id, n.name, endpoint, node.token)
-      #   end
+        if !resp.ok
+          invalid_reason = resp.node_responses[n.name].response
+          invalid_node = true
+          invalid_node_name = n.name
+          break
+        end
+
+        node = MoanaTypes::Node.from_json(resp.node_responses[n.name].response)
+        node.endpoint = endpoint
+        Datastore.create_node(pool.not_nil!.id, node.id, n.name, endpoint, node.token)
+      end
 
       if node.nil?
         invalid_node = true
