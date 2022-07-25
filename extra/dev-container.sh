8b26c61bf35e4eef396224d59d44b81cbbc26009
Aravinda Vishwanathapura <aravinda@kadalu.io>
2022-07-25 20:01:57 +0530

    Fix dev container name and expose the Mgr port to host machine (#212)
    Signed-off-by: Aravinda Vishwanathapura <aravinda@kadalu.tech>

--- extra/dev-container.sh
+++ extra/dev-container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -v /sys/fs/cgroup/:/sys/fs/cgroup:ro --privileged -v $PWD:/src -w /src --name kadalu-dev --hostname kadalu-dev kadalu/storage-node
+docker run -d -v /sys/fs/cgroup/:/sys/fs/cgroup:ro --privileged -p 3000:3000 -v $PWD:/src -w /src --name kadalu-dev --hostname kadalu-dev kadalu/storage-node-testing
